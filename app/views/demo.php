41ece2f9e2fb56cbb557946c9f910e9396fc5e19
alexnaev <102225427+alexnaev@users.noreply.github.com>
2022-03-31 18:34:57 +0200

    Programmed app and written source page

--- app/views/demo.php
+++ app/views/demo.php
@@ -9,11 +9,11 @@ require_once 'shared/header.php';
         <div class="row">
             <form class="col p-2 calcForm">
                 <label for="amount">Loan Amount</label>
-                <input type="number" id="amount" name="amount" class="form-control mb-2" aria-label="Loan Amount" value="15000">
+                <input type="number" id="amount" name="amount" class="form-control mb-2" aria-label="Loan Amount" value="150000">
                 <label for="term">Payments</label>
                 <input type="number" id="term" name="term" class="form-control mb-2" aria-label="Payments" value="60">
                 <label for="rate">Rate</label>
-                <input type="number" id="rate" name="rate" class="form-control mb-2" aria-label="Rate" value="3">
+                <input type="number" id="rate" name="rate" class="form-control mb-2" aria-label="Rate" value="10">
                 <div class="d-flex justify-content-end">
                     <button type="button" id="submit" class="btn btn-outline-dark">Calculate</button>
                 </div>
@@ -22,17 +22,17 @@ require_once 'shared/header.php';
                 <div class="row">
                     <div class="col-12 text-center mb-5">
                         <p class="h5"><em>Your Monthly Payments</em></p>
-                        <p class="h1"></p>
+                        <p class="h1" id="monthlyPayment"></p>
                     </div>
                     <div class="col">
-                        <p class="mb-0">Total Pricipal</p>
+                        <p class="mb-0">Total Principal</p>
                         <p class="mb-0">Total Interest</p>
                         <p class="mb-0">Total Cost</p>
                     </div>
                     <div class="col">
-                        <p class="mb-0"></p>
-                        <p class="mb-0"></p>
-                        <p class="mb-0"></p>
+                        <p class="mb-0" id="totalPrincipal"></p>
+                        <p class="mb-0" id="totalInterest"></p>
+                        <p class="mb-0" id="totalCost"></p>
                     </div>
                 </div>
             </div>
@@ -40,22 +40,39 @@ require_once 'shared/header.php';
         <!-- Result Table -->
         <div class="row mt-3">
             <div class="col-12 px-0">
-                <table class="table table-striped">
-                    <thead class="tableHeader">
-                        <th>Month</th>
-                        <th>Payment</th>
-                        <th>Principal</th>
-                        <th>Interest</th>
-                        <th>Total Interest</th>
-                        <th>Balance</th>
-                    </thead>
-                    <tbody id="rersultTable"></tbody>
-                </table>
+                <template id="tableTemplate">
+                    <tr>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                        <td></td>
+                    </tr>
+                </template>
+                <div class="table-responsive">
+                    <table class="table table-striped">
+                        <thead class="tableHeader">
+                            <th>Month</th>
+                            <th>Payment</th>
+                            <th>Principal</th>
+                            <th>Interest</th>
+                            <th>Total Interest</th>
+                            <th>Balance</th>
+                        </thead>
+                        <tbody id="resultTable">
+                            <!-- Results -->
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
 </main>
-
+<script src="js/site.js"></script>
+<script>
+    document.querySelector('#submit').addEventListener('click', getValues);
+</script>
 <?php
 require_once 'shared/footer.php';
 ?>
